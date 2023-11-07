a025f236b5c7d39b4b7c2ca5c61835a559b08f35
StefanoMela <stefano.mela25@gmail.com>
2023-11-07 18:13:58 +0100

    feat/ PJController fnc show updated

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -15,7 +15,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::select('id', 'type_id', 'title', 'description')
+        $projects = Project::select('id', 'type_id', 'title', 'description', 'slug')
         ->with('type:id,label,color', 'technologies:id,label,color')
         ->paginate(10);
 
@@ -30,7 +30,12 @@ class ProjectController extends Controller
      */
     public function show($id)
     {
-        //
+        $project = Project::select('id', 'type_id', 'title', 'description', 'slug')
+        ->where('id', $id)
+        ->with('type:id,label,color', 'technologies:id,label,color')
+        ->first();
+        
+        return response()->json($project);
     }
 
 }
